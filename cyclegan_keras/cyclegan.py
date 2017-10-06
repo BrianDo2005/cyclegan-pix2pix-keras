e6ca8ff8998b1315a2b9493ac99ad90616df3af3
Blake Dewey <blake.dewey@jhu.edu>
2017-10-06 08:49:08 -0400

    step printout off by 1

--- cyclegan_keras/cyclegan.py
+++ cyclegan_keras/cyclegan.py
@@ -161,7 +161,7 @@ class CycleGAN(object):
                     losses.extend(iter_losses)
                     iter_losses = []
                     time_per_img = (time.time() - iter_start_time) / batch_size
-                    message = '(Pre-Train, iter: %d, time: %.3f) ' % (step+1, time_per_img)
+                    message = '(Pre-Train, iter: %d, time: %.3f) ' % (step, time_per_img)
                     for name, loss in zip(self.loss_names, mean_iter_loss):
                         message += '%s: %.3f ' % (name, loss)
                     print(message)
